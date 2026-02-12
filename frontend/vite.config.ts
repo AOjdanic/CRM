00c14df571e94ece9fb3fbe7f94c1e4486c12e02
Aleksa Ojdanic <aojdanic97@gmail.com>
2026-02-12 22:39:33 +0100

    wip: trying to enable hmr in vite/docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:true,
-    port:3000
-  }
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+    },
+  },
 })
