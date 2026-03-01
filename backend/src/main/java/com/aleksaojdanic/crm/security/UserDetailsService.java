9d5aadf6dd69f9db282820cd051f61157b1b293c
Aleksa Ojdanic <aojdanic97@gmail.com>
2026-03-01 21:28:18 +0100

    created a UserDetailsService

---
+++ backend/src/main/java/com/aleksaojdanic/crm/security/UserDetailsService.java
@@ -0,0 +1,25 @@
+package com.aleksaojdanic.crm.security;
+
+import com.aleksaojdanic.crm.models.User;
+import com.aleksaojdanic.crm.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(()->new UsernameNotFoundException("User Not Found"));
+
+        return new SecurityUser(user);
+    }
+}
