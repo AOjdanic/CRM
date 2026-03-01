0801cde5ccc2993cc1100e75907e2fce4cb2992d
Aleksa Ojdanic <aojdanic97@gmail.com>
2026-03-01 19:47:57 +0100

    created User db entity

---
+++ backend/src/main/java/com/aleksaojdanic/crm/models/User.java
@@ -0,0 +1,29 @@
+package com.aleksaojdanic.crm.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Entity
+@Builder
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
