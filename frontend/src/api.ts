ce7178f0fa18f55328744fea20d112df570d0268
Aleksa Ojdanic <aojdanic97@gmail.com>
2026-03-08 00:37:39 +0100

    add auth token to all ongoin requests

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -4,4 +4,14 @@ const APIService = axios.create({
   baseURL: 'http://localhost:8080/api',
 });
 
+
+APIService.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default APIService;
