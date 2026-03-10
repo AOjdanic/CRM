9c3e6480e058c291612f12d374ae8dffb01fbbf7
Aleksa Ojdanic <aojdanic97@gmail.com>
2026-03-10 02:29:57 +0100

    add date fields to User model

--- backend/src/main/java/com/aleksaojdanic/crm/models/User.java
+++ backend/src/main/java/com/aleksaojdanic/crm/models/User.java
@@ -2,6 +2,11 @@ package com.aleksaojdanic.crm.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Setter
 @Getter
@@ -21,9 +26,18 @@ public class User {
     @Column(nullable = false)
     private String lastName;
 
-    @Column(unique = true,nullable = false)
+    @Column(unique = true, nullable = false)
     private String email;
 
     @Column(nullable = false)
     private String password;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @CreatedBy
+    private Long createdBy;
 }
