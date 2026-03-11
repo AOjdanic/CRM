6171f0eeea6b5409cbf48198b2539a45699ba341
Aleksa Ojdanic <aojdanic97@gmail.com>
2026-03-11 02:46:46 +0100

    replace @Autowired in favor of explicit constructor

--- backend/src/main/java/com/aleksaojdanic/crm/services/TodoService.java
+++ backend/src/main/java/com/aleksaojdanic/crm/services/TodoService.java
@@ -3,15 +3,15 @@ package com.aleksaojdanic.crm.services;
 import com.aleksaojdanic.crm.dto.TodoDto;
 import com.aleksaojdanic.crm.models.Todo;
 import com.aleksaojdanic.crm.repositories.TodoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TodoService {
-    @Autowired
-    private TodoRepository todoRepository;
+    private final TodoRepository todoRepository;
 
     public List<Todo> getTodos() {
         return todoRepository.findAll();
